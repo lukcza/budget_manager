0cd8e3cff5081cd34348d60638c65405cd1acbff
lukcza <lukcza23@gmail.com>
2025-03-17 15:15:53 +0100

    .

---
+++ lib/pages/add_payment.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+class AddPayment extends StatefulWidget {
+  const AddPayment({super.key});
+
+  @override
+  State<AddPayment> createState() => _AddPaymentState();
+}
+final List<DropdownMenuEntry<String>> dropdownItems = [
+  DropdownMenuEntry(value: '1', label: 'Opcja 1'),
+  DropdownMenuEntry(value: '2', label: 'Opcja 2'),
+  DropdownMenuEntry(value: '3', label: 'Opcja 3'),
+];
+class _AddPaymentState extends State<AddPayment> {
+  @override
+  String? selectedValue;
+  final amountController = TextEditingController();
+  final descriptionController = TextEditingController();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          DropdownMenu(
+              initialSelection: dropdownItems.first.value,
+              onSelected: (String? value) {
+                setState(() {
+                  selectedValue = value;
+                });
+              },
+              dropdownMenuEntries: dropdownItems),
+          TextField(
+            keyboardType: TextInputType.number,
+            controller: amountController,
+          ),
+          TextField(
+            keyboardType: TextInputType.text,
+          ),
+          ElevatedButton(
+              onPressed: ()=>print("payment send"),
+              child: Text("send"))
+        ],
+      ),
+    );
+  }
+}
